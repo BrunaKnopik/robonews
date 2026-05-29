7c555c4722c71dcfceb075b9acd8f40c726911c1
BrunaKnopik <brunaknopik@gmail.com>
2026-05-29 17:56:45 -0300

    robonews v1

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,9 @@ export default defineConfig({
   cloudflare: false,
   tanstackStart: {
     server: { entry: "server" },
+    prerender: {
+      enabled: true,
+      autoStaticPathsDiscovery: true,
+    },
   },
 });
